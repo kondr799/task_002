f317caac44e0bf9f2d1b53b6567edc36f7e0c8c0
kondr799 <83312253+kondr799@users.noreply.github.com>
2023-03-19 19:11:32 +0300

    home_work
    Signed-off-by: kondr799 <83312253+kondr799@users.noreply.github.com>

--- task_06/Program.cs
+++ task_06/Program.cs
@@ -7,7 +7,7 @@ Console.Clear();
 Console.WriteLine("Введите число: ");
 int a = Convert.ToInt32(Console.ReadLine());
 
-if (a % 2 == 0) ;
+if (a % 2 == 0)
 
-Console.WriteLine("Число является четным" + a + ": ");
+    Console.WriteLine("Число является четным" + a + ": ");
 Console.WriteLine($"Число является нечетным {a}");
