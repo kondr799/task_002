99c4ff5c9dc2d28d2e0feb4e88bd79ecbcf4dfcd
kondr799 <83312253+kondr799@users.noreply.github.com>
2023-03-21 01:01:37 +0300

    task_013_home
    Signed-off-by: kondr799 <83312253+kondr799@users.noreply.github.com>

---
+++ task_013/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+
+Console.Clear();
+Console.Write("Введите число: ");
+int value = Convert.ToInt32(Console.ReadLine());
+int length = value.ToString().Length;
+
+if (length >= 3)
+{
+    while (value > 999)
+    {
+        value = value / 10;
+    }
+    int result = value % 10;
+    Console.WriteLine("Третья цифра: " + result);
+}
+else
+{
+    Console.WriteLine("Число меньше трёх");
+}
+
