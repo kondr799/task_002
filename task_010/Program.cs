832692fc8327c01d346c8eab22132870cf026d83
kondr799 <83312253+kondr799@users.noreply.github.com>
2023-03-21 00:44:08 +0300

    task_010_home
    Signed-off-by: kondr799 <83312253+kondr799@users.noreply.github.com>

---
+++ task_010/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+Console.Clear();
+Console.WriteLine("Введите трёхзначное число: ");
+int value = Convert.ToInt32(Console.ReadLine());
+int length = value.ToString().Length;
+if (length == 3)
+{
+    int result = value / 10 % 10;
+    Console.WriteLine("Введите второе число: " + result);
+}
+else
+{
+    Console.WriteLine("Не указано трёхзначное число");
+}
