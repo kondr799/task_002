8a92a189dfa79efb535b27d34d5f292d25b3fc5d
kondr799 <83312253+kondr799@users.noreply.github.com>
2023-03-23 09:05:17 +0300

    task_023_home

--- task_023/Program.cs
+++ task_023/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 23 Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+
+Console.Clear();
+Console.Write("Введите число N = ");
+
+int n = Convert.ToInt32(Console.ReadLine());
+
+for (int i = 0; i <= n; i++)
+{
+    Console.Write($"{Math.Pow(i, 3)}");
+}
