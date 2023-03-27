cccf03df9be7b0e3939b4dada51524b002a21b79
kondr799 <83312253+kondr799@users.noreply.github.com>
2023-03-28 00:12:31 +0300

    task_027_home

---
+++ task_027/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+Console.Clear();
+
+Console.Write("Введите любое число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int res = 1;
+for (int i = 1; i < n; i++)
+{
+    res += n % 10;
+    n /= 10;
+}
+Console.WriteLine($"Сумма цифр = {res}");
