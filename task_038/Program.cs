85c5d5470d57799f2d1c39e925dcebca15056c57
kondr799 <83312253+kondr799@users.noreply.github.com>
2023-03-31 01:42:01 +0300

    task_038_home

---
+++ task_038/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 38: Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
+
+// [3 7 22 2 78] -> 76
+
+Console.Clear();
+
+Double[] array = new Double[8];
+
+for (int i = 0; i < array.Length; i++)
+    array[i] = Math.Round(new Random().NextDouble() * (50 - 10) + 10, 2);
+
+    double max1 = array[0];
+    double min1 = array[0];
+
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i] > max1)
+    max1 = array[i];
+    if (array[i] < min1)
+    min1 = array[i];
+}
+Console.WriteLine($"[{string.Join(", ", array)}]");
+Console.WriteLine(max1 - min1);
