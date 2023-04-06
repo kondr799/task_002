5212e1b5ebc84eb7b023b5107f0022ce33704c74
kondr799 <83312253+kondr799@users.noreply.github.com>
2023-04-07 00:30:27 +0300

    task_050_home

--- task_050/Program.cs
+++ task_050/Program.cs
@@ -8,4 +8,35 @@
 
 Console.Clear();
 
+Console.Write("Введите размер массива x: ");
+int x = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите размер массива y: ");
+int y = Convert.ToInt32(Console.ReadLine());
+double[,] RandomArray = new double[x,y];
+Console.Write("Введите позицию строки: ");
+int x2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите позицию столбца: ");
+int y2 = Convert.ToInt32(Console.ReadLine());
 
+void mas(int x, int y)
+{
+    int i,j;
+    Random randarin = new Random();
+    for (i = 0; i < x; i++)
+    {
+        Console.WriteLine();
+        for (j = 0; j < y; j++)
+        {
+            RandomArray[i,j] = randarin.NextDouble();
+            Console.Write($"{RandomArray[i,j]:F2}");
+        }
+        Console.WriteLine();
+    }
+}
+
+mas(x, y);
+if(x2 < 1 || y2 < 1)
+Console.Write("Позиции строк не могут быть отрицательными");
+else if (x2 <= x + 1 && y2 <= y + 1)
+Console.Write($"Значение равно {RandomArray[x2 - 1, y2 - 1]:F2}");
+else Console.Write("Элемента в массиве нет");
